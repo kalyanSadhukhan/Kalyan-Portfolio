b8cabc09b102a1ac6ccfe90762f282132411c299
kalyanSadhukhan <150598203+kalyanSadhukhan@users.noreply.github.com>
2026-03-11 20:21:11 +0530

    Update CORS configuration to allow all origins

--- backend/src/main/java/com/kalyan/portfolio/config/CorsConfig.java
+++ backend/src/main/java/com/kalyan/portfolio/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8083")   // frontend port
+                .allowedOrigins("*")   // frontend port
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
