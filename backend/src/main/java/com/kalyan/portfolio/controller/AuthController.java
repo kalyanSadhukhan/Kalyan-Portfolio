c6dfb46565123845b9ba32e8bffd644baf2276b7
kalyanSadhukhan <150598203+kalyanSadhukhan@users.noreply.github.com>
2026-03-11 22:24:27 +0530

    Remove CrossOrigin annotation from AuthController
    Removed CrossOrigin annotation for AuthController.

--- backend/src/main/java/com/kalyan/portfolio/controller/AuthController.java
+++ backend/src/main/java/com/kalyan/portfolio/controller/AuthController.java
@@ -8,7 +8,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:8083")
 public class AuthController {
 
     private final AuthService authService;
