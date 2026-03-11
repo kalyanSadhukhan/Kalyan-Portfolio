8d74d99dbaaf2b6f36fd7f65ca8fbccc23be940b
kalyanSadhukhan <150598203+kalyanSadhukhan@users.noreply.github.com>
2026-03-11 21:26:50 +0530

    Update CORS configuration to use allowedOriginPatterns

--- backend/src/main/java/com/kalyan/portfolio/config/CorsConfig.java
+++ backend/src/main/java/com/kalyan/portfolio/config/CorsConfig.java
@@ -9,15 +9,14 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-       registry.addMapping("/**")
-        .allowedOrigins(
-            "http://localhost:8083",
-            "https://kalyan-portfolio-steel.vercel.app"
-        )
-        .allowedMethods("*")
-        .allowedHeaders("*")
-        .allowCredentials(true)
+        registry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "http://localhost:8083",
+                        "https://kalyan-portfolio-steel.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
                 .maxAge(3600);
-
     }
 }
